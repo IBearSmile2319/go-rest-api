80ba9f76171e7fac0b5aa4f433aa81bf490bd943
maicol manuel orosco vasquez <maicolgamer23@hotmail.com>
2022-01-30 16:15:46 -0500

    Structuring our application

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,6 +4,22 @@ import (
 	"fmt"
 )
 
+// App - the struct which contains things like the pinters
+// to database connections
+type App struct {
+}
+
+// Run - sets up our application
+func (app *App) Run() error {
+	fmt.Println("Setting UP our server")
+	return nil
+}
+
 func main() {
 	fmt.Println("Go Rest Api Server")
+	app := App{}
+	if err := app.Run(); err != nil {
+		fmt.Println("Error starting up our server")
+		fmt.Println(err)
+	}
 }
