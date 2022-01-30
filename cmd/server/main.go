0d66c98ad6e719e01a2adbcade7e1d3d79c7a5f2
maicol manuel orosco vasquez <maicolgamer23@hotmail.com>
2022-01-30 16:32:02 -0500

    Added health check endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+
+	transportHTTP "github.com/IBearSmile2319/go-rest-api/internal/transport/http"
 )
 
 // App - the struct which contains things like the pinters
@@ -12,6 +15,14 @@ type App struct {
 // Run - sets up our application
 func (app *App) Run() error {
 	fmt.Println("Setting UP our server")
+
+	// Setup our routes
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		fmt.Println("Failed to set up server")
+		return err
+	}
 	return nil
 }
 
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/IBearSmile2319/go-rest-api
 
 go 1.17
+
+require github.com/gorilla/mux v1.8.0 // indirect
